daad5fc337ad2ee2b940ff14d63711fd4382ea38
RockRockWhite <1834309151@qq.com>
2023-09-07 22:25:56 +0800

    instructions: mv, lb, sb

--- asm/instruction.py
+++ asm/instruction.py
@@ -1,8 +1,9 @@
 import pin
 
 MV = 0
-LA = 1
+LB = 1
 LI = 2
+SB = 3
 
 ADD = 10
 SUB = 11
@@ -15,10 +16,26 @@ NOP = 62
 HLT = 63
 
 instructions = {
-    MV: [pin.SRC_R | pin.DST_S, pin.CYC_RS],
-    LA: [],
+    # mv r0, r1
+    MV: [pin.SRC_S | pin.DST_S, pin.CYC_RS],
+    # lb r0, 0x55aa
+    # load byte from memory
+    LB: [
+        pin.SRC_R | pin.MAR_W,
+        pin.MC_R | pin.DST_S,
+        pin.CYC_RS,
+    ],
     # li r0, 0x55aa
     LI: [pin.SRC_R | pin.DST_S, pin.CYC_RS],
+    # sb r0, 0x55aa
+    # sb srt, dst
+    # revert in asm!!!
+    SB: [
+        pin.SRC_S | pin.MDR_W,
+        pin.DST_R | pin.MAR_W,
+        pin.MC_W,
+        pin.CYC_RS,
+    ],
     ADD: [],
     SUB: [],
     AND: [],
